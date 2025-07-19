fbfec47eb3eccf1676a8670839442b461a62731a
Leopold <leopolddag@gmail.com>
2025-07-19 23:58:11 +0200

    dev1

--- testgit.java
+++ testgit.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 class testgit {
     public static void main(String[] args) {
-        System.out.println("Hello, DEV! again with revert!");
+        System.out.println("Hello, dev1! again with revert!");
         
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your name: ");
