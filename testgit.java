0a52a67e01ca8ad89767f39c5f8734c1b0b1592f
Leopold <leopolddag@gmail.com>
2025-07-19 10:07:45 +0200

    commit dev1

--- testgit.java
+++ testgit.java
@@ -1,5 +1,5 @@
 class testgit {
     public static void main(String[] args) {
-        System.out.println("Hello, Git depuis dev1 et 2!");
+        System.out.println("Hello, Git nouveau test dev1!");
     }
 }
