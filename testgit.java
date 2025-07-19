747c9b5245e4efb68a75966d1ae234ce7432238c
Leopold <leopolddag@gmail.com>
2025-07-19 23:48:44 +0200

    adding scanner

--- testgit.java
+++ testgit.java
@@ -2,6 +2,29 @@ import java.util.Scanner;
 
 class testgit {
     public static void main(String[] args) {
-        System.out.println("Hello, Git nouveau test dev2!");
+        System.out.println("Hello, DEV! again with revert!");
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine(); // Décommenté pour attendre l'entrée utilisateur
+        // String name = "John"; // Commenté pour ne pas attribuer une valeur fixe
+
+        switch (name.toLowerCase()) {
+            case "john":
+                System.out.println("Hello, John!");
+                break;
+            case "leopold":
+                System.out.println("Hello, Leopold!");
+                break;
+            case "alice":
+                System.out.println("Hello, Alice!");
+                break;
+            default:
+                System.out.println("Personne ne vous connais !");
+                break;
+        }
+
+        scanner.close();
+        System.out.println("Goodbye, " + name + "!");
     }
 }
