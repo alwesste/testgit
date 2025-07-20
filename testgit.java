21a48219fe5e98f0f27f4e1929ac58375263b0cb
Leopold <leopolddag@gmail.com>
2025-07-20 17:52:12 +0200

    5:52

--- testgit.java
+++ testgit.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 class testgit {
     public static void main(String[] args) {
-        System.out.println("Hello world, today!");
+        System.out.println("bonjour dev1!");
         //test
 
         Scanner scanner = new Scanner(System.in);
